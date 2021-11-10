43193969ed9a48e33c87177049144ae7d5d03abb
alex <allexander0509@gmail.com>
2021-11-10 22:53:03 +0300

    Changed setup.py, added calculator modules

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup, find_packages
 
 setup(name="homework0", packages=find_packages())
-setup(name="homework1", packages=find_packages())
+setup(name="calculator", packages=find_packages())

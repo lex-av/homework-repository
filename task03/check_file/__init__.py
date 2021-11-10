795065a378918b137b2f36e2f50023c46b5301f0
alex <allexander0509@gmail.com>
2021-11-10 23:39:22 +0300

    task03 initialised

---
+++ task03/check_file/__init__.py
@@ -0,0 +1,12 @@
+# Created by allex at 10.11.2021
+
+# Enter feature description here
+# -*- coding: utf-8 -*-
+
+
+def functionality():
+    return
+
+
+if __name__ == '__main__':
+    functionality()

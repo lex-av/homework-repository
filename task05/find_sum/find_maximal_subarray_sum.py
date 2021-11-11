688ad14d86769d41951c0ce8a73b7990859eba0f
alex <allexander0509@gmail.com>
2021-11-11 22:09:19 +0300

    Task05 finished realisation

--- task05/find_sum/find_maximal_subarray_sum.py
+++ task05/find_sum/find_maximal_subarray_sum.py
@@ -12,4 +12,25 @@ from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    ...
+    """Using windowing method"""
+
+    # Maximums
+    global_max = -float("inf")  # Lowest possible number to compare (for negative array cases)
+    local_max = []
+
+    # Sliding
+    for window_size in range(k, 0, -1):
+        for window_border in range(window_size, len(nums) + 1):
+            local_max.append(sum(nums[window_border - window_size:window_border]))
+
+        # Maximum from all sliding window sums
+        maximum = max(local_max)
+        if maximum > global_max:
+            global_max = maximum
+        local_max = []
+
+    return global_max
+
+
+a = [-1, 0, 0, 10, 25, 25, 25]
+print(find_maximal_subarray_sum(a, 1))

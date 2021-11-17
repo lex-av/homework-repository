4c648e8d5e17ab199677e2c816d61cf2b08cda8b
alex <allexander0509@gmail.com>
2021-11-17 11:57:55 +0300

    Task 5 realisation modified

--- homework2/pack_hw5/module_hw5.py
+++ homework2/pack_hw5/module_hw5.py
@@ -12,18 +12,54 @@ assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', '
 
 
 from io import TextIOWrapper
-from typing import Any, List
+from typing import Any, AnyStr, List, Union
 
 
-def custom_range(iterable: Any, *args: int) -> List:
+def custom_range(iterable: Any, *args: Union[int, AnyStr]) -> List:
+    """Converts given iterable to [optionally sliced] list.
+
+    Slicing pivots for file object corresponds to character
+    position in file and should be integers.
+    Slicing pivots for tuple and list works normally and
+    should be integers.
+    Slicing pivots for string may be str or int. So,
+    slicing by index and element is available.
+
+    Dict will be converted to list of keys
+
+    Set wil be converted to list"""
+
+    # If file object given
     if isinstance(iterable, TextIOWrapper):
         iterable = list(map(str.strip, iterable.readlines()))
+
+        if args:
+            return iterable[args[0]:args[1]:args[2]]
+        else:
+            return iterable
+
+    # Str object given
+    elif isinstance(iterable, str):
+        elems_to_indexes = dict(zip(list(iterable), range(len(iterable))))
+        iterable = list(iterable)
+
+        if args:
+            # Slicing for str args
+            if isinstance(args[0], str) and isinstance(args[0], str) and isinstance(args[0], str):
+                return iterable[elems_to_indexes[args[0]]:elems_to_indexes[args[1]]:elems_to_indexes[args[2]]]
+            # Slicing for int args
+            elif isinstance(args[0], int) and isinstance(args[0], int) and isinstance(args[0], int):
+                return iterable[args[0]:args[1]:args[2]]
+        else:
+            raise Exception("Got wrong args for slicing")
+
+    # Tuple/list/Set/Iterator object given
     else:
         iterable = list(iterable)
-    if args:
-        return iterable[args[0]:args[1]:args[2]]
-    else:
-        return iterable
+        if args:
+            return iterable[args[0]:args[1]:args[2]]
+        else:
+            return iterable
 
 
 if __name__ == '__main__':

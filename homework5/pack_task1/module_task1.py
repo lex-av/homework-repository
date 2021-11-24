9558ec2f30952eb731738b85a4d212712668fecf
alex <allexander0509@gmail.com>
2021-11-24 17:27:37 +0300

    Homework 5 task 1 done, added empty test file

--- homework5/pack_task1/module_task1.py
+++ homework5/pack_task1/module_task1.py
@@ -34,25 +34,69 @@ PEP8 соблюдать строго.
 К названием остальных переменных, классов и тд. подходить ответственно -
 давать логичные подходящие имена.
 """
-# import datetime
+import datetime
+import time
+
+
+class Student:
+    """Class for student definition"""
+
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def do_homework(self, homework):
+        if homework.is_active():
+            return homework
+        print("You are late")
+        return None
+
+
+class Teacher:
+    """Class for teacher definition"""
+
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def create_homework(self, text, deadline):
+        return Homework(text, deadline)
+
+
+class Homework:
+    """Class for homework definition and deadline check"""
+
+    def __init__(self, text, deadline):
+        self.text = text
+        self.deadline = deadline
+        self.created = datetime.datetime.now()
+
+    def is_active(self):
+        """Checks if homework task is still active and deadline isn't expired'"""
+        current_datetime = datetime.datetime.now()
+        if current_datetime <= self.created + datetime.timedelta(self.deadline):
+            return True
+        return False
 
 
 if __name__ == "__main__":
-    ...
-    # teacher = Teacher('Daniil', 'Shadrin')
-    # student = Student('Roman', 'Petrov')
-    # teacher.last_name  # Daniil
-    # student.first_name  # Petrov
-    #
-    # expired_homework = teacher.create_homework('Learn functions', 0)
-    # expired_homework.created  # Example: 2019-05-26 16:44:30.688762
-    # expired_homework.deadline  # 0:00:00
-    # expired_homework.text  # 'Learn functions'
-    #
-    # # create function from method and use it
-    # create_homework_too = teacher.create_homework
-    # oop_homework = create_homework_too('create 2 simple classes', 5)
-    # oop_homework.deadline  # 5 days, 0:00:00
-    #
-    # student.do_homework(oop_homework)
-    # student.do_homework(expired_homework)  # You are late
+    """Examples here"""
+
+    teacher = Teacher("Daniil", "Shadrin")
+    student = Student("Roman", "Petrov")
+    print(teacher.last_name)  # Daniil
+    print(student.first_name)  # Petrov
+
+    expired_homework = teacher.create_homework("Learn functions", 0)
+    print(expired_homework.created)  # Example: 2019-05-26 16:44:30.688762
+    print(expired_homework.deadline)  # 0:00:00
+    print(expired_homework.text)  # 'Learn functions'
+
+    # create function from method and use it
+    create_homework_too = teacher.create_homework
+    oop_homework = create_homework_too("create 2 simple classes", 5)
+    print(oop_homework.deadline)  # 5 days, 0:00:00
+
+    student.do_homework(oop_homework)
+    time.sleep(2)  # Need to wait a bit
+    student.do_homework(expired_homework)  # You are late

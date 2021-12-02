6956ebec8fb17ab29aa59640bf349e5bc88711aa
alex <allexander0509@gmail.com>
2021-12-02 16:15:06 +0300

    Homework1 fixed tests data paths

--- homework1/tests/test_task3.py
+++ homework1/tests/test_task3.py
@@ -2,12 +2,12 @@ from ..pack_task3.module_task3 import find_maximum_and_minimum
 
 
 def test_positive_1():
-    assert find_maximum_and_minimum("tst_data/src_1.txt") == (-1, 10)
+    assert find_maximum_and_minimum("tests/tst_data/src_1.txt") == (-1, 10)
 
 
 def test_positive_2():
-    assert find_maximum_and_minimum("tst_data/src_2.txt") == (-100, 2)
+    assert find_maximum_and_minimum("tests/tst_data/src_2.txt") == (-100, 2)
 
 
 def test_positive_3():
-    assert find_maximum_and_minimum("tst_data/src_3.txt") == (12, 50)
+    assert find_maximum_and_minimum("tests/tst_data/src_3.txt") == (12, 50)

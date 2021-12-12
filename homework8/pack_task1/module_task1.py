62a77ada13f335e10a3b53cdbcf82f398953979b
alex <allexander0509@gmail.com>
2021-12-12 23:16:56 +0300

    Homework8 task1 first implementation. Need more logic and tests

--- homework8/pack_task1/module_task1.py
+++ homework8/pack_task1/module_task1.py
@@ -23,6 +23,46 @@ attribute (for example when there's a line 1=something) ValueError should be rai
 to be small, you are permitted to read it entirely into memory.
 """
 
+from collections import Hashable
+
+
+class KeyValueStorage:
+    """Tool for converting text files into dict-like structure"""
+
+    key_value_storage = {}
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.keys_values_raw = []
+
+        with open(filepath) as source:
+            for line in source:
+                # Here will be A LOT of logic
+                key, value = tuple(line.strip().split("="))
+                if isinstance(key, Hashable):
+                    self.key_value_storage[key] = value
+
+    def __setitem__(self, key, value):
+        self.key_value_storage[key] = value
+
+    def __getitem__(self, key):
+        if key in self.key_value_storage:
+            return self.key_value_storage[key]
+        else:
+            raise Exception("Key doesn't exist")
+
+    def __delitem__(self, key):
+        if key in self.key_value_storage:
+            del self.key_value_storage[key]
+        else:
+            raise Exception("Software Name doesn't exist")
+
+    def __len__(self):
+        return len(self.key_value_storage)
+
 
 if __name__ == "__main__":
-    ...
+    new_storage = KeyValueStorage("data.txt")
+    new_storage["abc"] = 123
+
+    print()

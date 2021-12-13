e0be980660d9534eedc4b3ab9390d7703c428f4a
alex <allexander0509@gmail.com>
2021-12-13 22:40:38 +0300

    Homework8 task2 files initialised

---
+++ homework8/pack_task2/module_task2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Task definition format is too complex for comment
+Gotta leave a link here:
+https://github.com/lex-av/epam_python_autumn_2020/blob/main/lecture_08_object_model/hw/task2.rst
+"""
+
+
+def functionality():
+    return
+
+
+if __name__ == "__main__":
+    functionality()

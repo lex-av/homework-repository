99314d4f07c586719d8e908e7c98f43195eaa3ba
alex <allexander0509@gmail.com>
2021-12-16 16:23:51 +0300

    Homework9 task 1 added tests and passed them

--- homework9/pack_task1/module_task1.py
+++ homework9/pack_task1/module_task1.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Write a function that merges integer from sorted files and returns an iterator
-file1.txt:
+task1_file1.txt:
 1
 3
 5
-file2.txt:
+task1_file2.txt:
 2
 4
 6
 
-list(merge_sorted_files(["file1.txt", "file2.txt"]))
+list(merge_sorted_files(["task1_file1.txt", "task1_file2.txt"]))
 [1, 2, 3, 4, 5, 6]
 
 """
@@ -79,7 +79,7 @@ def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
 
 
 if __name__ == "__main__":
-    merge_iter = MergingIterator(["file1.txt", "file2.txt", "file3.txt"])
+    merge_iter = merge_sorted_files(["file1.txt"])
 
-    a = [i for i in merge_iter]
+    merged_lst = [i for i in merge_iter]
     print()
---
+++ homework9/tests/test_task1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from ..pack_task1.module_task1 import merge_sorted_files
+
+
+def test_positive_read_one_file():
+    path_to_test = os.getcwd()
+    filepath = path_to_test + "/homework9/tests/tests_data/task1_file1.txt"
+    assert list(merge_sorted_files([filepath])) == [1, 3, 5]
+
+
+def test_positive_merge_several_file():
+    path_to_test = os.getcwd()
+    file_paths = [
+        path_to_test + "/homework9/tests/tests_data/task1_file1.txt",
+        path_to_test + "/homework9/tests/tests_data/task1_file2.txt",
+        path_to_test + "/homework9/tests/tests_data/task1_file3.txt",
+    ]
+    assert list(merge_sorted_files(file_paths)) == [0, 1, 1, 2, 2, 2, 3, 4, 5, 6, 10, 110]
+
+
+def test_positive_read_empty_file():
+    path_to_test = os.getcwd()
+    filepath = path_to_test + "/homework9/tests/tests_data/task1_file4.txt"
+    assert list(merge_sorted_files([filepath])) == []
+
+
+def test_positive_merge_empty_file():
+    path_to_test = os.getcwd()
+    file_paths = [
+        path_to_test + "/homework9/tests/tests_data/task1_file1.txt",
+        path_to_test + "/homework9/tests/tests_data/task1_file4.txt",
+    ]
+    assert list(merge_sorted_files(file_paths)) == [1, 3, 5]

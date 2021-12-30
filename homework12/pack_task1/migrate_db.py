2f3fde61de0f652c91a74fccd1ce96499ddd4084
alex <allexander0509@gmail.com>
2021-12-30 23:23:44 +0300

    Homework 12 task 1 added some code ti migration

--- homework12/pack_task1/migrate_db.py
+++ homework12/pack_task1/migrate_db.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import playhouse.migrate as pm
 
-if __name__ == "__main__":
-    pass
+my_db = pm.SqliteDatabase("main.db")
+migrator = pm.SqliteMigrator(my_db)
+
+title_field = pm.CharField(default="")
+status_field = pm.IntegerField(null=True)
+
+pm.migrate(
+    migrator.add_column("some_table", "title", title_field),
+    migrator.add_column("some_table", "status", status_field),
+    migrator.drop_column("some_table", "old_column"),
+)

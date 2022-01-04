0043b2a3e581b0ccfbe21d51e7bf7757e431813e
alex <allexander0509@gmail.com>
2022-01-04 19:18:48 +0300

    Homework 12 changed db name

--- homework12/app_task1/app_task1/settings.py
+++ homework12/app_task1/app_task1/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "app_task1.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "main.db",
     }
 }
 

94fb7da0d517248b78984763057b3fc1e9dadb1e
alex <allexander0509@gmail.com>
2022-01-04 21:34:34 +0300

    Homework 12 refactor: moved task files to separate django project 'learning_data'. DB structure reset

--- homework12/app_task1/app_task1/settings.py
+++ homework12/app_task1/app_task1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app_task1",
+    "learning_data",
 ]
 
 MIDDLEWARE = [
--- homework12/app_task1/learning_data/models.py
+++ homework12/app_task1/learning_data/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Create your models here.
 
 import datetime
 

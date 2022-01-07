f3bc4c6291a23b2fccc48747625d8987ac6c15b4
alex <allexander0509@gmail.com>
2022-01-07 20:11:21 +0300

    Homework3 task3 fixed tests

--- homework3/tests/test_task3.py
+++ homework3/tests/test_task3.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from ..pack_task3.module_task3 import make_filter
+from homework3.pack_task3.module_task3 import make_filter
 
 sample_data_1 = [
     {
@@ -10,12 +10,7 @@ sample_data_1 = [
         "occupation": "was here",
         "type": "person",
     },
-    {
-        "is_dead": True,
-        "kind": "parrot",
-        "type": "bird",
-        "name": "polly"
-    }
+    {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
 ]
 
 sample_data = [
@@ -25,21 +20,15 @@ sample_data = [
         "occupation": "was here",
         "type": "person",
     },
-    {
-        "is_dead": True,
-        "kind": "parrot",
-        "type": "bird",
-        "name": "polly"
-    }
+    {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
 ]
 
 
 def test_make_filter_sample_data():
-    assert make_filter(name='Bill').apply(sample_data_1) == [{'name': 'Bill',
-                                                              'last_name': 'Gilbert',
-                                                              'occupation': 'was here',
-                                                              'type': 'person'}]
+    assert make_filter(name="Bill").apply(sample_data_1) == [
+        {"name": "Bill", "last_name": "Gilbert", "occupation": "was here", "type": "person"}
+    ]
 
 
 def test_make_filter_sample_data_key_missing():
-    assert make_filter(namee='Bill').apply(sample_data_1) == []
+    assert make_filter(namee="Bill").apply(sample_data_1) == []

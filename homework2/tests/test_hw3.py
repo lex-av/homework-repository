d475f939c7a73fea8aad8ef4f93e7a1e1923397c
alex <allexander0509@gmail.com>
2022-01-07 16:06:37 +0300

    Homework2 task3 fixed tests

--- homework2/tests/test_hw3.py
+++ homework2/tests/test_hw3.py
@@ -1,22 +1,41 @@
 # -*- coding: utf-8 -*-
-from ..pack_hw3.module_hw3 import combinations
+
+import pytest
+
+from homework2.pack_hw3.module_hw3 import combinations
+
+expected_list_1 = [[1, 3], [1, 4], [2, 3], [2, 4]]
+expected_list_2 = [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]]
+expected_list_3 = [[1, 3, 5], [1, 3, 6], [1, 4, 5], [1, 4, 6], [2, 3, 5], [2, 3, 6], [2, 4, 5], [2, 4, 6]]
+expected_list_4 = [
+    [1, 3, 5, 7],
+    [1, 3, 5, 8],
+    [1, 3, 6, 7],
+    [1, 3, 6, 8],
+    [1, 4, 5, 7],
+    [1, 4, 5, 8],
+    [1, 4, 6, 7],
+    [1, 4, 6, 8],
+    [2, 3, 5, 7],
+    [2, 3, 5, 8],
+    [2, 3, 6, 7],
+    [2, 3, 6, 8],
+    [2, 4, 5, 7],
+    [2, 4, 5, 8],
+    [2, 4, 6, 7],
+    [2, 4, 6, 8],
+]
+
+test_data = [
+    ([[1, 2], [3, 4]], expected_list_1),
+    ([[1, 2, 3], [4, 5, 6]], expected_list_2),
+    ([[1, 2], [3, 4], [5, 6]], expected_list_3),
+    ([[1, 2], [3, 4], [5, 6], [7, 8]], expected_list_4),
+]
 
 
-def test_combinations():
+@pytest.mark.parametrize("args, expected", test_data)
+def test_combinations(args, expected):
     """Testing cases"""
-    assert combinations([1, 2], [3, 4]) == [[1, 3], [1, 4], [2, 3], [2, 4]]
 
-    assert combinations([1, 2, 3], [4, 5, 6]) == [[1, 4], [1, 5], [1, 6], [2, 4],
-                                                  [2, 5], [2, 6], [3, 4], [3, 5],
-                                                  [3, 6]]
-
-    assert combinations([1, 2], [3, 4], [5, 6]) == [[1, 3, 5], [1, 3, 6], [1, 4, 5],
-                                                    [1, 4, 6], [2, 3, 5], [2, 3, 6],
-                                                    [2, 4, 5], [2, 4, 6]]
-
-    assert combinations([1, 2], [3, 4], [5, 6], [7, 8]) == [[1, 3, 5, 7], [1, 3, 5, 8], [1, 3, 6, 7],
-                                                            [1, 3, 6, 8], [1, 4, 5, 7], [1, 4, 5, 8],
-                                                            [1, 4, 6, 7], [1, 4, 6, 8], [2, 3, 5, 7],
-                                                            [2, 3, 5, 8], [2, 3, 6, 7], [2, 3, 6, 8],
-                                                            [2, 4, 5, 7], [2, 4, 5, 8], [2, 4, 6, 7],
-                                                            [2, 4, 6, 8]]
+    assert combinations(*args) == expected

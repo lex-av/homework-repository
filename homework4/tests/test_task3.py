ce10e73ffa477d935a1494690d13bbb671b943ea
alex <allexander0509@gmail.com>
2022-01-08 01:21:11 +0300

    Homework4 task3 fixed variable names for stderr and stdout

--- homework4/tests/test_task3.py
+++ homework4/tests/test_task3.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-from ..pack_task3.module_task3 import my_precious_logger
+from homework4.pack_task3.module_task3 import my_precious_logger
 
 
 def test_my_precious_logger_stderr(capsys):
-    my_precious_logger('Error: !!!')
-    captured = capsys.readouterr()
-    message = captured[1]
-    assert message == 'Error: !!!\n'
+    my_precious_logger("Error: !!!")
+    captured_error_msg = capsys.readouterr()
+    message = captured_error_msg[1]
+    assert message == "Error: !!!\n"
 
 
 def test_my_precious_logger_stdout(capsys):
-    my_precious_logger('OK')
-    captured = capsys.readouterr()
-    message = captured[0]
-    assert message == 'OK\n'
+    my_precious_logger("OK")
+    captured_stdout_msg = capsys.readouterr()
+    message = captured_stdout_msg[0]
+    assert message == "OK\n"

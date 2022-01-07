68392cf937156656a9e5bf7f7c981e581d561799
alex <allexander0509@gmail.com>
2022-01-07 19:15:48 +0300

    Homework2 task5 test negative step fix

--- homework2/tests/test_hw5.py
+++ homework2/tests/test_hw5.py
@@ -15,7 +15,7 @@ test_data_3 = [
     ("abcdefgh", ["a", "h"], ["a", "b", "c", "d", "e", "f", "g"]),
     ("abcdefgh", ["a", "f"], ["a", "b", "c", "d", "e"]),
     ("abcdefgh", ["a", "f", 2], ["a", "c", "e"]),
-    ("abcdefgh", [None, None, -1], ["h", "g", "f", "e", "d", "c", "b", "a"]),
+    ("abcdefgh", ["f", "a", -1], ["f", "e", "d", "c", "b"]),
 ]
 
 

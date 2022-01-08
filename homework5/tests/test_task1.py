e994895428c94183427e0707e2499308b1179755
alex <allexander0509@gmail.com>
2022-01-08 17:49:05 +0300

    Homework5 task1 fixed tests, applied freezegun stuff

--- homework5/tests/test_task1.py
+++ homework5/tests/test_task1.py
@@ -2,23 +2,23 @@
 
 import datetime
 
-from ..pack_task1.module_task1 import Homework, Student, Teacher
+from homework5.pack_task1.module_task1 import Homework, Student, Teacher
 
 
-def test_positive_homework_is_active():
+def test_pos_homework_is_active():
     """Checks if deadline calculation is correct on synthetic dates"""
 
     new_hw = Homework("Homework 1: do sample", 2)
     assert new_hw.is_active()
 
 
-def test_negative_homework_is_active():
+def test_pos_homework_is_active_false_output(freezer):
     """Checks if deadline calculation is correct on synthetic dates"""
 
-    new_hw = Homework("Homework 1: do sample", 0)
-    new_hw.created = new_hw.created - datetime.timedelta(
-        2
-    )  # Shuffle created parameter 2 days back
+    new_hw = Homework("Homework 1: do sample", 2)
+    current_date = datetime.date.today()
+    expired_date = current_date + datetime.timedelta(3)
+    freezer.move_to(expired_date.strftime("%d-%m-%Y"))
     assert not new_hw.is_active()
 
 
@@ -39,13 +39,15 @@ def test_positive_student_do_active_homework():
     assert hw_result is not None
 
 
-def test_negative_student_do_non_active_homework():
+def test_student_do_non_active_homework(freezer):
     """Checks that result of doing non-active homework is none"""
     teacher = Teacher("Daniil", "Shadrin")
-    new_homework = teacher.create_homework("New homework: do it", 0)
-    new_homework.created = new_homework.created - datetime.timedelta(
-        2
-    )  # Shuffle created parameter 2 days back
+    new_homework = teacher.create_homework("New homework: do it", 2)
+
+    current_date = datetime.date.today()
+    expired_date = current_date + datetime.timedelta(3)
+    freezer.move_to(expired_date.strftime("%d-%m-%Y"))
+
     student = Student("Roman", "Petrov")
     hw_result = student.do_homework(new_homework)
 

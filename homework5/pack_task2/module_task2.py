426864b0c8b09d91882e88eddefd26c4d6693637
alex <allexander0509@gmail.com>
2022-01-08 17:26:25 +0300

    Homework5 task2 fixed implementation using nested decorator, united test cases

--- homework5/pack_task2/module_task2.py
+++ homework5/pack_task2/module_task2.py
@@ -14,7 +14,7 @@ print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 """
 
 import functools
-from functools import wraps
+from inspect import signature
 
 
 def parameters_capture(func):
@@ -28,11 +28,35 @@ def parameters_capture(func):
     return wrapper
 
 
+def deep_wraps(func):
+    def inner_wraps(deco_wrapper):
+        def wrapper(*args, **kwargs):
+            # could assign original func attrs to its initial deco
+            # but it seems no need for that
+
+            # deco_wrapper.__name__ = func.__name__
+            # deco_wrapper.__doc__ = func.__doc__
+            # deco_wrapper.__original_func = func
+            # deco_wrapper.__signature__ = signature(func)
+
+            # Assign needed attrs to wrapper of deep_wraps, because it's
+            # attrs will be called eventually
+            wrapper.__name__ = func.__name__
+            wrapper.__doc__ = func.__doc__
+            wrapper.__original_func = func
+            wrapper.__signature__ = signature(func)
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return inner_wraps
+
+
 def print_result(func):
 
     # Place for new decorator
-    @parameters_capture
-    @wraps(func)
+    @deep_wraps(func)  # Takes original func and its decorator
     def wrapper(*args, **kwargs):
         """Function-wrapper which print result of an original function"""
         result = func(*args, **kwargs)
@@ -54,7 +78,10 @@ if __name__ == "__main__":
 
     print(custom_sum.__doc__)
     print(custom_sum.__name__)
+
     without_print = custom_sum.__original_func
 
+    print("Down from here should be done")
     # the result returns without printing
     without_print(1, 2, 3, 4)
+    print("done")
--- homework5/tests/test_task2.py
+++ homework5/tests/test_task2.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from ..pack_task2.module_task2 import custom_sum
+from homework5.pack_task2.module_task2 import custom_sum
 
 
-def test_parameters_capture_decorator_func_doc():
+def test_deep_wraps_func_doc():
     custom_sum(1, 2, 3, 4)
-    assert custom_sum.__doc__ == "This function can sum any objects which have __add___"
-
-
-def test_parameters_capture_decorator_func_name():
-    custom_sum(1, 2, 3, 4)
-    assert custom_sum.__name__ == "custom_sum"
+    assert (
+        custom_sum.__doc__ == "This function can sum any objects which have __add___"
+        and custom_sum.__name__ == "custom_sum"
+    )

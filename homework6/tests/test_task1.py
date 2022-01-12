c8a1ea76619d500e20c14bd745620c5cef86ef73
alex <allexander0509@gmail.com>
2022-01-12 18:40:16 +0300

    Homework6 task2 got rid of fixtures in tests

--- homework6/tests/test_task1.py
+++ homework6/tests/test_task1.py
@@ -1,57 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-
 from ..pack_task1.module_task1 import User
-
-
-@pytest.fixture
-def create_one_class_instance():
-    user_0 = User("Smith")
-    yield user_0
-    User.reset_instances_counter()
-
-
-@pytest.fixture
-def create_multiple_class_instances():
-    _, _, user_b = User("Smith"), User("Smith"), User("Smith")
-    yield user_b
-    User.reset_instances_counter()
-
-
-@pytest.fixture
-def create_multiple_class_instances_for_reset_return():
-    _, _, user_b = User("Smith"), User("Smith"), User("Smith")
-    yield user_b
-    User.reset_instances_counter()
-
-
-@pytest.fixture
-def create_multiple_class_instances_for_reset_result():
-    _, _, user_b = User("Smith"), User("Smith"), User("Smith")
-    yield user_b
-    User.reset_instances_counter()
 
 
 def test_positive_instances_counter_no_instances():
     assert User.get_created_instances() == 0
 
 
-def test_positive_instances_counter_just_one_instance(create_one_class_instance):
-    assert create_one_class_instance.get_created_instances() == 1
+def test_positive_instances_counter_just_one_instance():
+    user_0 = User("Smith")
+    count = user_0.get_created_instances()
+    User.reset_instances_counter()
+    assert count == 1
 
 
-def test_positive_instances_counter_multiple_instances(create_multiple_class_instances):
-    assert create_multiple_class_instances.get_created_instances() == 3
+def test_positive_instances_counter_multiple_instances():
+    _, _, user_b = User("Smith"), User("Smith"), User("Smith")
+    count = user_b.get_created_instances()
+    User.reset_instances_counter()
+    assert count == 3
 
 
-def test_positive_instances_counter_reset_return(create_multiple_class_instances_for_reset_return):
-    assert create_multiple_class_instances_for_reset_return.reset_instances_counter() == 3
+def test_positive_instances_counter_reset_return():
+    _, _, user_b = User("Smith"), User("Smith"), User("Smith")
+    assert user_b.reset_instances_counter() == 3
 
 
-def test_positive_instances_counter_reset_result(create_multiple_class_instances_for_reset_result):
-    create_multiple_class_instances_for_reset_result.reset_instances_counter()
-    assert create_multiple_class_instances_for_reset_result.get_created_instances() == 0
+def test_positive_instances_counter_reset_result():
+    _, _, user_b = User("Smith"), User("Smith"), User("Smith")
+    user_b.reset_instances_counter()
+    count = user_b.reset_instances_counter()
+    assert count == 0
 
 
 def test_pos_instances_counter_with_deeper_inheritance():
